8424e3599f10e1a6ed5b1801a96ca63cf6ca00b3
Boseong00 <qhtjdvv3351@gmail.com>
2025-07-01 17:49:58 +0900

    모임 장소 병합중

--- src/main/java/com/example/ocean/controller/place/ViewController.java
+++ src/main/java/com/example/ocean/controller/place/ViewController.java
@@ -1,16 +1,10 @@
 package com.example.ocean.controller.place;
 
-import com.example.ocean.domain.Place;
+
 import com.example.ocean.service.PlaceService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 @Controller //뷰 반환 컨트롤러
 public class ViewController {
