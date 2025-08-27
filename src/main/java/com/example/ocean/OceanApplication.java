369e11da9dd4e823afd5d8112570948f4c6529cf
윤현기 <dbsgusrl1234567@naver.com>
2025-08-28 00:16:09 +0900

    no message

--- src/main/java/com/example/ocean/OceanApplication.java
+++ src/main/java/com/example/ocean/OceanApplication.java
@@ -3,17 +3,15 @@ package com.example.ocean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@MapperScan({
-		"com.example.ocean.mapper",
-		"com.example.ocean.repository"
-})
+@MapperScan("com.example.ocean.mapper")  // repository 제거!
+@EnableJpaRepositories(basePackages = "com.example.ocean.repository")  // JPA용 명시적 설정
+@EntityScan(basePackages = "com.example.ocean.entity")  // Entity 스캔 명시
 public class OceanApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OceanApplication.class, args);
 	}
-
 }
